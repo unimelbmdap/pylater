531733126e424ed8e17ac3c471d65268cba4226b
Damien Mannion <damien@djmannion.net>
2024-04-13 17:58:17 +1000

    -

--- src/pylater/plot.py
+++ src/pylater/plot.py
@@ -21,11 +21,17 @@ def format_reciprobit_axes(
     y_min=0.001,
     y_max=1 - 0.001,
 ) -> None:
+    ax.set_xlim(0.1,0.5)
     ax.set_ylim(bottom=y_min, top=y_max)
     ax.set_yscale("function", functions=(probit_forward, probit_inverse))
     ax.set_xscale("function", functions=(invert, invert))
 
+    #sec = ax.secondary_xaxis("top") #, functions=(invert, invert))
+    #sec.set_xlim(0.1,0.5)
+    #sec.set_xscale("linear") #, functions=(i, i))
+    #sec.set_xticks((ax.get_xticks()))
 
+def i(values): return 1/values
 
 def invert(values: npt.ArrayLike) -> npt.NDArray[np.float_]:
     return -1 / np.array(values)
