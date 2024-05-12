6bf61a1decd22bed3fa59e3fc8900809bd1ecd9f
Damien Mannion <damien@djmannion.net>
2024-05-12 10:25:32 +1000

    -

--- src/pylater/model.py
+++ src/pylater/model.py
@@ -1,7 +1,108 @@
+from __future__ import annotations
+
+import enum
+import typing
+
+import numpy as np
+
 import pymc as pm
 
 import pylater.data
 import pylater.dist
+
+
+class ShareType(enum.Enum):
+    SHIFT = "shift"
+    SWIVEL = "swivel"
+
+
+def build_default_model(
+    datasets: typing.Sequence[pylater.data.Dataset],
+    share_type: ShareType | None = None,
+) -> pm.Model:
+
+    n_datasets = len(datasets)
+
+    dataset_names = [dataset.name for dataset in datasets]
+
+    if share_type is ShareType.SHIFT:
+        n_sigma = 1
+        n_k = n_datasets
+        sigma_dims = "shared"
+        k_dims = "dataset"
+    elif share_type is ShareType.SWIVEL:
+        n_sigma = n_datasets
+        n_k = 1
+        sigma_dims = "dataset"
+        k_dims = "shared"
+    else:
+        n_sigma = n_k = n_datasets
+        sigma_dims = k_dims = "dataset"
+
+    with pm.Model(
+        coords={
+            "dataset": dataset_names,
+            "shared": ("shared",),
+        },
+    ) as model:
+
+        # 95% CI of [0.375, 1.5]
+        sigma = pm.LogNormal(
+            "sigma",
+            mu=np.log(0.75),
+            sigma=np.log(2) / 2,
+            dims=sigma_dims,
+        )
+
+        sigma_all = pm.math.pt.broadcast_to(
+            x=sigma,
+            shape=(n_datasets,),
+        )
+
+        # 95% CI of [2.5, 10]
+        k = pm.LogNormal(
+            "k",
+            mu=np.log(5),
+            sigma=np.log(2) / 2,
+            dims=k_dims,
+        )
+
+        k_all = pm.math.pt.broadcast_to(
+            x=k,
+            shape=(n_datasets,),
+        )
+
+        # 95% CI of [2, 8]
+        sigma_e_mod = pm.LogNormal(
+            "sigma_e_mod",
+            mu=np.log(4),
+            sigma=np.log(2) / 2,
+            dims="dataset",
+        )
+
+        mu = pm.Deterministic(
+            "mu",
+            sigma_all * k_all,
+            dims="dataset",
+        )
+
+        sigma_e = pm.Deterministic(
+            "sigma_e",
+            sigma_all * sigma_e_mod,
+            dims="dataset",
+        )
+
+        for (i_dataset, dataset) in enumerate(datasets):
+
+            pylater.LATER(
+                name=f"obs_{dataset.name}",
+                mu=mu[i_dataset],
+                sigma=sigma[i_dataset],
+                sigma_e=sigma_e[i_dataset],
+                observed_rt_s=dataset.rt_s,
+            )
+
+    return model
 
 
 def demo():
